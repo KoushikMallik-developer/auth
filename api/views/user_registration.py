f8987a270951044ac716872576c73c0c578be219
KMallik <koushikmallik001@gmail.com>
2023-03-22 00:59:40 +0530

    Adding Serializers. Getting error "Apps are not loaded yet"

--- api/views/user_registration.py
+++ api/views/user_registration.py
@@ -1,7 +1,13 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from api.serializers.user_registration_serializer import UserRegistrationSerializer
 
 
 class UserRegistrationView(APIView):
     def post(self, request):
-        return Response({"msg": "Registration Successful"})
+        user_reg_serializer = UserRegistrationSerializer(data=request.data)
+        if user_reg_serializer.is_valid(raise_exception=True):
+            user_reg_serializer.save()
+            return Response({"msg": "Registration Successful"}, status=status.HTTP_201_CREATED)
+        return Response({"msg": "Registration Failed"}, status=status.HTTP_400_BAD_REQUEST)
